98bfe7f55e354c3c1844f08c58a332ce34616bd2
Roger Henry <rhenry8@asu.edu>
2025-07-13 11:15:15 -0700

    Completed part 2

--- client/main.js
+++ client/main.js
@@ -45,9 +45,86 @@ function renderList(agents) {
 }
 
 
+// HTTP Methods
+
+// Create a new agent
+function doPost(agent) {
+    const init = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(agent)
+    }
+
+    // Make POST request
+    fetch('http://localhost:8080/api/agent', init)
+        .then(response => {
+            if (response.status === 201 || response.status === 400){        // 201 = created successfully, 400 = validation error
+                return response.json();
+            } else {
+                return Promise.reject(`Unexpected Status Code: ${response.status}`);
+            }
+        })
+        .then(data => {
+            if (data.agentId) {             // Success, refresh the list and reset the form
+                displayList();
+                resetState();
+            } else {
+                renderError(data);          // Validation error - display them
+            }
+        })
+        .catch(console.log);                // Log any expections
+}
+
 // CRUD functions
+
+// Handle form submission
+function handleSubmit(event) {
+    event.preventDefault();     // prevents page reload
+
+    // Get form values
+    const agent = {
+        firstName: document.getElementById('firstName').value,
+        middleName: document.getElementById('middleName').value,
+        lastName: document.getElementById('lastName').value,
+        dob: document.getElementById('dob').value,
+        heightInInches: parseInt(document.getElementById('heightInInches').value)
+    };
+
+    if (editAgentId > 0) {
+        agent.agentId = editAgentId;
+        doPut(agent);
+    } else {
+        doPost(agent);
+    }
+}
 
 // Handle adding a new agent
 function handleAddAgent() {
     setCurrentView('Form');
+}
+
+// Handle delete agent
+function handleDeleteAgent(agentId) {
+    // Find agent with matching ID
+    const agent = agents.find(a => a.agentId === agentId);
+
+    // Confirm deletion
+    if (confirm(`Delete Agent ${agent.firstName} ${agent.lastName}?`)) {
+        const init = {
+            method: 'DELETE'
+        };
+
+        // Make DELETE API request
+        fetch(`http://localhost:8080/api/agent/${agentId}`, init)
+            .then(response => {
+                if (response.status === 204) {          // Success, refresh the list
+                    displayList();                  
+                } else {
+                    return Promise.reject(`Unexpected Status Code ${response.status}`);     // Unexpected error
+                }
+            })
+            .catch(console.log);
+    }
 }
